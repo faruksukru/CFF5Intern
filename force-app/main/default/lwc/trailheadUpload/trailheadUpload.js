997729ce5bdcf9705d9e40ba55ee259b0d11d1d5
fatihemrenur <fatihemrenur@gmail.com>
2024-03-13 10:54:56 +0300

    Commit_detail information_13032024

--- force-app/main/default/lwc/trailheadUpload/trailheadUpload.js
+++ force-app/main/default/lwc/trailheadUpload/trailheadUpload.js
@@ -4,24 +4,27 @@ import deleteRecord from '@salesforce/apex/ApplicantApexController.deleteRecord'
 import updateUpload from '@salesforce/apex/ApplicantApexController.updateUpload';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class TrailheadUpload extends LightningElement {
-
-@track applicantemail=null;
+// variables used in Js
+@track applicantemail=null;//initial value for email
 @track applicant;
-@track trailheadRecordId;
-upload=false;
+@track trailheadRecordId;//keeps newly created trailhead record
+upload=false;//used for upload part in HTML to e shown
 isExist=false;
-isShowModal=false;
-Modal1=false;
-Modal2=false;
+isShowModal=false;//used for modal to be shown
+Modal1=false;//used in modal to show first option (we have screen shoot)
+Modal2=false;//used in modal to show second option (screen shoot uploded succesfully)
 @track label=null;//used in modal in HTML to show which heading is used in modal
 acceptedFormats = ['.pdf', '.png', '.jpg', '.jpeg'];//restrict upload formats
-
+//To get entered email
 handleInput(event) {
 this.applicantemail = event.target.value; 
 }
-
+/*call apex conttroller to check if we have this email in applicant object records. 
+if yes, then it looks if trailhead uplod field is checked, if checked it gives message, if unchecked
+it create a new record in trailhead object to get screen shoot. if we dont ant record related with email, 
+the sytem gives another message says we couldnt find email try a new one. 
+Here we have change value of the boolean variable to show correct messsages in modal in HTML */
 handleSave() {
-console.log('Test1'+ this.applicantemail);
 getApplicant({eml: this.applicantemail})
 .then(result => {
 this.applicant = result[0];
@@ -60,23 +63,22 @@ this.label='modal-heading-01';
 this.Modal1=true;
 this.Modal2=false;
 this.isShowModal=true;
-
 // Optionally, you can reset the form or perform other actions after record creation
 this.resetForm();  
 });
 }
-
+//this reset email input
 resetForm() {
 this.applicantEmail =null;
 //window.location.reload();
 }
-
+/*When screen shoot uploades succesfully, it shows sucess message and change boolean variables based on the condition to be shown on the Modal.
+Also call apex controler to update trailhead upload check box.*/ 
 handleUploadFinished(event) {
-//const uploadedFiles = event.detail.files;
-// Process the uploaded files, e.g., show a success message
+updateUpload({ recordEmail: this.applicantemail })
+.then(result => {
 const toastEvent = new ShowToastEvent({
 title: 'Success!',
-// message: `${uploadedFiles.length} files uploaded successfully.`,
 message: 'Your file uploaded successfully!',
 variant: 'success',
 });
@@ -86,58 +88,39 @@ this.label='modal-heading-02';
 this.Modal2=true;
 this.Modal1=false;
 this.isShowModal=true;
-
-updateUpload({ recordEmail: this.applicantemail })
-    .then(result => {
-    /*const toastEvent = new ShowToastEvent({
-    title: 'We have deleted your previous Screen Shot successfully!',
-    message: 'You can Upload new Screen Shoot again by using your Email',
-    variant: 'success',
-    });
-    this.dispatchEvent(toastEvent);
-    // Optionally, you can reset the form or perform other actions after record creation
-    this.resetForm();*/
-    })
-    .catch(error => {
-    // Handle error, show error message, etc.
-    console.error('erroris'+error);
-   /* const toastEvent = new ShowToastEvent({
-    title: 'We have an error while deleting your Screen Shot. Please Contact with CFF',
-    message: {error},
-    variant: 'error',
-    });
-    this.dispatchEvent(toastEvent);
-    this.resetForm();*/
-    });    
-
+})
+.catch(error => {
+// Handle error, show error message, etc.
+console.log('error is'+JSON.stringify(error));
+});    
 }
-
+//This is called via delete button in modal to call apex controller to delete trailhead record and show messages about delete
 deleteModalBox() { 
-    deleteRecord({ recordEmail: this.applicantemail })
-    .then(result => {
-    const toastEvent = new ShowToastEvent({
-    title: 'We have deleted your previous Screen Shot successfully!',
-    message: 'You can Upload new Screen Shoot again by using your Email',
-    variant: 'success',
-    });
-    this.dispatchEvent(toastEvent);
-    // Optionally, you can reset the form or perform other actions after record creation
-    this.resetForm();
-    })
-    .catch(error => {
-    // Handle error, show error message, etc.
-    console.error('erroris'+error);
-    const toastEvent = new ShowToastEvent({
-    title: 'We have an error while deleting your Screen Shot. Please Contact with CFF',
-    message: {error},
-    variant: 'error',
-    });
-    this.dispatchEvent(toastEvent);
-    this.resetForm();
-    });    
- this.isShowModal = false;
+deleteRecord({ recordEmail: this.applicantemail })
+.then(result => {
+const toastEvent = new ShowToastEvent({
+title: 'We have deleted your previous Screen Shot successfully!',
+message: 'You can Upload new Screen Shoot again by using your Email',
+variant: 'success',
+});
+this.dispatchEvent(toastEvent);
+// Optionally, you can reset the form or perform other actions after record creation
+this.resetForm();
+})
+.catch(error => {
+// Handle error, show error message, etc.
+console.error('erroris'+error);
+const toastEvent = new ShowToastEvent({
+title: 'We have an error while deleting your Screen Shot. Please Contact with CFF',
+message: {error},
+variant: 'error',
+});
+this.dispatchEvent(toastEvent);
+this.resetForm();
+});    
+this.isShowModal = false;
 }
-
+//This is called Go CFF Website button in modal to go to web site of CFF
 exitModalBox() {  
 this.isShowModal = false;
 const websiteUrl = 'https://www.changeforcefoundation.org/'; 
@@ -146,5 +129,4 @@ const websiteUrl = 'https://www.changeforcefoundation.org/';
 // Option 2: Navigate in the same tab/window
 window.location.href = websiteUrl;
 }
-
 }
